f3b158a05bb3d6b22095ee8fc2e9d2f8b073a1a2
Eliram <eliram13@gmail.com>
2019-03-26 19:35:12 +0900

    creating read write options for blog posts

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,18 +2,50 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class BlogController extends AbstractController
 {
+    /** @var integer */
+    const POST_LIMIT = 5;
+
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    /** @var \Doctrine\Common\Persistence\ObjectRepository */
+    private $authorRepository;
+
+    /** @var \Doctrine\Common\Persistence\ObjectRepository */
+    private $blogPostRepository;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->blogPostRepository = $entityManager->getRepository('App:BlogPost');
+        $this->authorRepository = $entityManager->getRepository('App:Author');
+    }
+
     /**
      * @Route("/", name="homepage")
+     * @Route("/entries", name="entries")
      */
-    public function indexAction()
+    public function entriesAction(request $request)
     {
-        return $this->render('blog/index.html.twig',
-            ['controller_name' => 'blogController'
-        ]);
+        $page = 1;
+
+        if($request->get('page')) {
+            $page = $request->get('page');
+        }
+        return $this->render('blog/entries.html.twig',
+            [
+                'blogPosts' => $this->blogPostRepository->findAll()
+            ]
+        );
     }
 }
