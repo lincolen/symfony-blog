d364ff6ad72fbceb61ad36d899b6d77f1ccaa6c3
Eliram <eliram13@gmail.com>
2019-04-04 19:26:56 +0900

    setting up travis CI and heroku

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Http\HttplugBundle\HttplugBundle::class => ['all' => true],
     HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
